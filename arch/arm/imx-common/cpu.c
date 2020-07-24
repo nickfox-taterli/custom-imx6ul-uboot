865a87879e77c3d71b220b11ce04b3befdfee86c
TaterLi <admin@taterli.com>
2020-07-24 10:15:09 +0000

    test

--- arch/arm/imx-common/cpu.c
+++ arch/arm/imx-common/cpu.c
@@ -230,9 +230,9 @@ int print_cpuinfo(void)
 		if (!ret)
 			printf(" at %dC\n", cpu_tmp);
 		else
-			debug(" - invalid sensor data\n");
+			printf(" - invalid sensor data\n");
 	} else {
-		debug(" - invalid sensor device\n");
+		printf(" - invalid sensor device\n");
 	}
 #endif
 
